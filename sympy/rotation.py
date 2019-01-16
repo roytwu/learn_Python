fc46835e6a47ee13055046c4e721377a5273d0a0
roytwu <roywu2018@gmail.com>
2019-01-16 17:40:25 -0500

    adding matrix representation in SymPy

--- sympy/rotation.py
+++ sympy/rotation.py
@@ -1,16 +1,22 @@
-# -*- coding: utf-8 -*-
 """
 File name:   rottion.py
 Date:        Created on Wed Jan 16 16:36:31 2019
 Author:      wur
 Description: symbolic computations for rattion matrices
 """
-
+#import mpmath
 import sympy as sym
+from sympy          import symbols
+from sympy          import sin, cos, tan, cot
 from sympy.printing import pprint
-from sympy.matrices import *
+from sympy.matrices import Matrix
 
-I = Matrix([[1,0,0],[0,1,0], [0,0,1]])
+psi, phi, theta = symbols("psi phi theta")
+theta = sin(psi)+1
+pprint(theta)
 
-pprint(2*I)
-pprint(eye(3))
+
+
+I = Matrix([ [cos(psi),0,0],[0,1,0], [0,0,1] ])
+pprint(I)
+
