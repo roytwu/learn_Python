9173564306df558c0e2cd97dfbbae26557bd0bd6
roytwu <roywu2018@gmail.com>
2019-01-17 11:26:23 -0500

    adding symbolic computation about rotation matrices

--- sympy/rotation.py
+++ sympy/rotation.py
@@ -12,11 +12,17 @@ from sympy.printing import pprint
 from sympy.matrices import Matrix
 
 psi, phi, theta = symbols("psi phi theta")
-theta = sin(psi)+1
-pprint(theta)
+al, be, ga      = symbols("alpha beta gamma")
 
+Rx = Matrix([ [1,0,0], [0, cos(al), -sin(al)], [0, sin(al), cos(al)] ])
+Ry = Matrix([ [cos(be), 0, sin(be)], [0,1,0], [-sin(be), 0, cos(be)] ])
+Rz = Matrix([ [cos(ga), -sin(ga), 0], [sin(ga), cos(ga), 0], [0,0,1] ])
+#pprint(Rx)
 
+#* use simplify() to get simplest form of an expression
+pprint(simplify(Rx*Rx.T))
 
-I = Matrix([ [cos(psi),0,0],[0,1,0], [0,0,1] ])
-pprint(I)
+bRg = Rx*Rz*Ry
+#pprint(bRg)
+
 
