c44ffda537ec3b059fa0a3c84fd5e356f5ead7cf
roytwu <roywu2018@gmail.com>
2019-02-22 18:07:50 -0500

    adding one more class example

--- oop/class_basic.py
+++ oop/class_basic.py
@@ -4,7 +4,7 @@ Date:        Feb 22 2019
 Author:      wur
 Description: basic concepts about Python class
 """
-
+#* -----clss sample #1-----
 class ClassRoy:
   #* A sample example class
   i = 12345
@@ -21,14 +21,13 @@ while foo.counter < 10:
 
 print('Shall print out 16...')
 print(foo.counter)
-
 del foo.counter #* delete counter  
+
 
 print('\n')
 print(foo.hello())
 
-
-bar = foo.hello
+bar = foo.hello #* create a method object
 i = 0
 while True: 
     print(bar())
@@ -36,3 +35,30 @@ while True:
     
     if i == 9:
         break
+    
+#* -----clss sample #2----- 
+print('\n')        
+class Dog:
+    m_kind = 'canine' #* variable shared by all instances
+    
+    def __init__(self, name):
+        self.m_name = name #* variable unique to each instance 
+        self.m_tricks = []
+        
+    def add_trick(self, trick):
+        self.m_tricks.append(trick)
+
+o_fido = Dog('Fido')
+o_buddy = Dog('Buddy')
+print(o_fido.m_kind)
+print(o_fido.m_name)
+
+o_buddy.add_trick('roll over')
+print(o_buddy.m_tricks)
+
+
+
+
+
+
+    
