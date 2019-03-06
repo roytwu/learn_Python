ef7220420afb4c1ba58a6ccc5171367a304f627c
roytwu <roywu2018@gmail.com>
2019-03-06 10:56:09 -0500

    *renaming files *adding 'attitude' project

---
+++ sympy/attitude/SO3.py
@@ -0,0 +1,6 @@
+"""
+Developer:   Roy TWu
+File Name:   SO3.py
+Description: custom modules about attitude representation and transformation
+"""
+
---
+++ sympy/attitude/rodrigues.py
@@ -0,0 +1,20 @@
+"""
+File name:   rodrigues.py
+Developer:   wur
+Description: symbolic computations for Rodrigues formula
+"""
+from sympy          import symbols
+from sympy          import sin, cos, tan, cot
+from sympy          import asin, acos, atan, atan2
+from sympy.printing import pprint
+from sympy.matrices import Matrix
+from sympy          import simplify as simp
+
+
+#import SO3
+#
+#
+#if __name__ == "__main__":
+#  print("---------\n")
+#  pprint(attitude.Rxx)
+#  print("---------\n")
