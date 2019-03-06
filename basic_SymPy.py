c25e95337066af6790c01b043621f520999773b7
roytwu <roywu2018@gmail.com>
2019-03-06 14:37:48 -0500

    moving files around

---
+++ basic_SymPy.py
@@ -0,0 +1,44 @@
+"""
+Created on: Tue Jan 15 14:51:35 2019
+File name:  basic_SymPy.py
+Author:     wur
+"""
+
+import sympy as sym
+#sym.init_printing()
+
+a=sym.sqrt(3)
+print(a)
+
+#* in SymPy, variables are defined using symbols
+x, y = sym.symbols('x, y')
+ans1 = x + 2*y
+print(ans1, "\n")
+
+#* integrate
+print(sym.integrate(1/x, x))
+
+#* expanded form and factored form
+from sympy import expand, factor
+ans2 = expand(x*ans1)
+print(ans2)
+print(factor(ans2))
+
+ans3 = sym.diff(sym.sin(x)*sym.exp(x), x)
+ans4 = sym.Integral(sym.sqrt(1/x), x)
+
+# pretty-print
+sym.printing.pprint(ans2, ans3)
+
+
+#* import every object from SymPy
+from sympy import *
+printing.pprint(ans4)
+
+
+#* creating matrices in SymPy
+from sympy.printing import pprint
+from sympy.matrices import Matrix
+I = Matrix([[1,0,0],[0,1,0], [0,0,1]])
+pprint(2*I)
+pprint(eye(3))
---
+++ basic_rotation.py
@@ -0,0 +1,59 @@
+"""
+File name:   rotation.py
+Date:        Created on Wed Jan 16 16:36:31 2019
+Author:      wur
+Description: symbolic computations for rotation matrices
+"""
+#import mpmath
+import sympy as sym
+from sympy          import symbols
+from sympy          import sin, cos, tan, cot
+from sympy.printing import pprint
+from sympy.matrices import Matrix
+from sympy          import simplify as simp
+
+psi, phi, theta = symbols("psi phi theta")
+al, be, ga      = symbols("alpha beta gamma")
+
+Rx = Matrix([ [1,0,0], [0, cos(al), -sin(al)], [0, sin(al), cos(al)] ])
+Ry = Matrix([ [cos(be), 0, sin(be)], [0,1,0], [-sin(be), 0, cos(be)] ])
+Rz = Matrix([ [cos(ga), -sin(ga), 0], [sin(ga), cos(ga), 0], [0,0,1] ])
+#pprint(Rx)
+
+#* use simplify() to get simplest form of an expression
+pprint(Rx*Rx.T) 
+pprint(simplify(Rx*Rx.T)) 
+
+
+#* matrix transpose 
+bool = ( Rx.T == Rx.transpose() )
+print(bool, "\n")
+
+
+#* matrix inverse
+#bool = ( simp(Ry.inv("LU")) == Ry.T )
+bool = ( simp(Ry.inv()) == Ry.T )
+print(bool, "\n")
+"""
+By default, inv() is computed by Gaussian elimintion, 
+use inv("LU") to specify to use LU decomposition
+"""
+
+#* matrix determinant
+pprint( simp(Rz.det()) )
+print("\n")
+
+
+#* eigenvalues and eigenvectors
+#a=Rz.eigenvects()[1]
+#b=Rz.eigenvals().keys()
+#[Vec,D] = Rz.diagonalize()
+#pprint(Vec)
+#print( sqrt(Vec[0,1]**2 + Vec[1,1]**2 + Vec[2,1]**2) )
+#print(simp(Vec[0,1]*Vec[0,1]))
+
+
+bRg = Rx*Rz*Ry
+pprint(simp(bRg))
+
+
