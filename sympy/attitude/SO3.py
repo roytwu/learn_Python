0f3bf538713f07ead99c463061694ba56f25c1a7
roytwu <roywu2018@gmail.com>
2019-03-06 12:02:47 -0500

    adding symbolic Rodrigues formula

--- sympy/attitude/SO3.py
+++ sympy/attitude/SO3.py
@@ -4,3 +4,29 @@ File Name:   SO3.py
 Description: custom modules about attitude representation and transformation
 """
 
+from sympy          import symbols
+from sympy          import sin, cos, tan, cot
+from sympy          import asin, acos, atan, atan2
+from sympy.printing import pprint
+from sympy.matrices import Matrix
+from sympy          import simplify as simp
+
+th, phi, psi = symbols("theta phi psi")
+x, y, z      = symbols("x y z")
+v1, v2, v3   = symbols("v1 v2 v3")
+
+Rx = Matrix([ [1,0,0], [0, cos(th), -sin(th)], [0, sin(th), cos(th)] ])
+Ry = Matrix([ [cos(phi), 0, sin(phi)], [0,1,0], [-sin(phi), 0, cos(phi)] ])
+Rz = Matrix([ [cos(psi), -sin(psi), 0], [sin(psi), cos(psi), 0], [0,0,1] ])
+
+eye3 = Matrix([ [1,0,0], [0, 1, 0], [0, 0, 1] ])
+
+hat_v = Matrix([ [0,-v3, v2], [v3, 0, -v1], [-v2, v1, 0] ])
+
+#* hat map
+def hat(tup_v):
+    v1 = tup_v[0]
+    v2 = tup_v[1]
+    v3 = tup_v[2]
+    hatMap = Matrix([ [0,-v3, v2], [v3, 0, -v1], [-v2, v1, 0] ])   
+    return hatMap 
--- sympy/attitude/rodrigues.py
+++ sympy/attitude/rodrigues.py
@@ -11,10 +11,29 @@ from sympy.matrices import Matrix
 from sympy          import simplify as simp
 
 
-#import SO3
-#
-#
-#if __name__ == "__main__":
-#  print("---------\n")
-#  pprint(attitude.Rxx)
-#  print("---------\n")
+import SO3  #* use custom module
+eye3 = SO3.eye3
+
+
+th, phi, psi = symbols("theta phi psi")
+x, y, z      = symbols("x y z")
+a1, a2, a3   = symbols("a1 a2 a3")
+
+
+#* ===== Rodrigues Formula =====
+a = (a1, a2, a3)
+a_hat = SO3.hat(a)
+
+#* both Rod1 and Rod2 are Rodrigues formula in different form 
+#* compare them and print out the result
+Rod1 = cos(th)*eye3 + (1-cos(th))*(a_hat*a_hat+eye3) + sin(th)*a_hat
+Rod2 = eye3 + sin(th)*a_hat + (1-cos(th))*a_hat*a_hat
+
+result = simp(Rod1-Rod2)
+pprint(result)
+
+pprint(simp(Rod2))
+
+
+#* 
+xx = (x, 0, 0) 
