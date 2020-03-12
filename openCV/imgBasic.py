aaad3ba197f137a7a7b933b3bbf1795f8430bc68
roytwu <roywu2018@gmail.com>
2020-03-12 17:22:25 -0400

    adding comments

--- openCV/imgBasic.py
+++ openCV/imgBasic.py
@@ -1,6 +1,6 @@
 #* ----------------------------------------
 #* Author:      Roy Wu
-#* Description: Basic program of Python OpenCV
+#* Description: Simple program to read/write image with Python OpenCV
 #* ----------------------------------------
 import cv2
 import numpy as np
@@ -12,9 +12,6 @@ img = cv2.imread('sample/robot.png')
 print(img.shape)
 print(img.dtype)
 
-#b,g,r = cv2.split(img)
-#print("b is ...", b)
-
 
 px127 = img[127, 127]
 pxA   = img[30, 60]
@@ -23,10 +20,16 @@ print("pxA is ...", pxA)     #* shall be cyan (B,G,R)
 
 #* make window resizable 
 cv2.namedWindow('resized_Win', cv2.WINDOW_NORMAL)
+
 #* resize window
 h = img.shape[0] #* height
 w = img.shape[1] #* width
 cv2.resizeWindow('resized_Win', w*3, h*3)
-
 cv2.imshow('resized_Win', img)
+
+
+#* channel
+#(b, g, r) = cv2.split(img)
+blue = img[:, :, 0];
+cv2.imwrite('blue_channel.png', blue)
 
