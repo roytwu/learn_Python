54e0784e65ff505163018de251f25bb00bdef4fb
Roy <roywu2018@gmail.com>
2020-04-12 11:21:12 -0400

    saving progress

--- attitude/_basicRotation.py
+++ attitude/_basicRotation.py
@@ -8,7 +8,9 @@ from sympy          import sin, cos, tan, cot
 from sympy          import asin, acos, atan, atan2
 from sympy.printing import pprint
 from sympy.matrices import Matrix
+from sympy.matrices import Transpose
 from sympy          import simplify as simp
+import numpy as np
 
 import SO3  #* use custom module
 eye3 = SO3.eye3
@@ -22,10 +24,9 @@ a1, a2, a3   = symbols("a1 a2 a3")
 a = (a1, a2, a3)
 a_hat = SO3.hat(a)
 aa = SO3.vee(a_hat)
-pprint(a_hat)
-pprint(aa)
-print('\n')       
-
+#pprint(a_hat)
+#pprint(aa)
+#print('\n')       
 
 #* ----- Rodrigues Formula -----
 #* both Rod1 and Rod2 are Rodrigues formula in different form 
@@ -35,25 +36,34 @@ Rod2 = eye3 + sin(th)*a_hat + (1-cos(th))*a_hat*a_hat
 result = simp(Rod1-Rod2)
 #pprint(result)
 
-
-
 #* ----- rotation along basic axes -----
-Rz = SO3.Rz(psi)
-Ry = SO3.Ry(th) 
-Rx = SO3.Rx(phi)
-
+Rz = SO3.Rz(psi) #* 1st rotation; o  -> B'
+Ry = SO3.Ry(th)  #* 2nd rotation; B' -> B"
+Rx = SO3.Rx(phi) #* 3rd rotation; B" -> B
 R_OB = Rz*Ry*Rx
-
 #pprint(Rz)
 #pprint(Ry)
 #pprint(Rx)
-
-pprint(R_OB)
-
+#pprint(R_OB)
 
 
+#* ----- Kinematic singularity -----
+xBasis = Matrix([[1], [0], [0]])
+yBasis = Matrix([[0], [1], [0]])
+zBasis = Matrix([[0], [0], [1]])
 
+Bp_y = (Rz)*yBasis
+print("Bp_y is...")
+pprint(Bp_y)
 
+Bpp_x = Rz*Ry*xBasis
+print("Bpp_x is...")
+pprint(Bpp_x)
+
+print("\nCombine is...")
+combine = Matrix([[0, -sin(psi), cos(psi)*cos(th)], [0, cos(psi), sin(psi)*cos(th)], [1, 0, -sin(th)]])
+cInv = combine.inv()
+pprint(simp(cInv)) 
 
 
 
