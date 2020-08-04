69c1594ccf96928b4b1d4ff1bf4d59386c0e036c
Roy <roywu2018@gmail.com>
2020-08-04 12:57:11 -0400

    comparing exp() with expm()

--- basic/numpy_basic.py
+++ basic/numpy_basic.py
@@ -2,9 +2,26 @@
 Author:      Roy Wu
 Description: Basic programming for numpy
 """
-
-
 import numpy as np
-a = np.arange(15).reshape(3,5)
+from scipy.linalg import expm, sinm, cosm
+from pprint import pprint 
 
-print(a)
+# a = np.arange(15).reshape(3,5) #* 3 rows x 5 columns
+a = np.arange(9).reshape(3,3) #* 3 rows x 5 columns
+m1 = np.exp(a) 
+ma = expm(a)
+
+print('Matrix a is...\n', a)
+print('\nElement-wise exponential is ...\n', m1)
+print('\nMatrix exponential is... ')
+pprint(ma)
+
+
+#*---------- ----------
+#*     Euler's identity  
+#*---------- ----------
+eu1 = expm(1j*a)
+eu2 = np.exp(1j*a)
+eu3 = cosm(a) + 1j*sinm(a)
+print(eu1-eu2)
+print(eu1-eu3) #* this shall output zero matrix
