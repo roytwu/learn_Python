dac47c2ff97ad184354f3ce3c8dbea62edf26725
Roy <roywu2018@gmail.com>
2020-09-28 02:00:01 -0400

    adding inverseK process

--- basic/planarRR.py
+++ basic/planarRR.py
@@ -7,7 +7,7 @@ import numpy             as np
 import matplotlib.pyplot as plt
 from numpy        import linalg
 from scipy.linalg import expm, sinm, cosm
-from math import sin, cos, acos, atan, atan2
+from math import sin, cos, acos, atan, atan2, sqrt
 from math import radians, degrees
  
 
@@ -17,7 +17,9 @@ from math import radians, degrees
 l1=3     #* link 1
 l2=1     #* link 2
 th1=60   #* joint variable 1 in degrees
-th2=60  #* joint variable 2 in degrees
+th2=45  #* joint variable 2 in degrees
+# th1=81.598
+# th2=-45  
 th1 = radians(th1) #* convert degree to radians
 th2 = radians(th2)
 x = l1*cos(th1) +l2*cos(th1+th2)
@@ -39,17 +41,40 @@ print('theta2 is...', t2)
 
 
 #*---------- ----------
-#*    arc-tangent
+#*    arc-tangent -1
 #*---------- ----------
-dum1 = math.sqrt(1-d**2)
+dum1 = sqrt(1-d**2)
 dum2 = d 
 toto = degrees(atan(dum1/dum2))
 print('toto is...', toto)
 
+
 #*---------- ----------
-#*    atan2
+#*    arc-tangent -2
 #*---------- ----------
-tata = degrees(atan2(dum1, dum2))
-print('tata is...', tata)
+duma =((l1+l2)**2 - (x**2+y**2))/((x**2+y**2)-(l1-l2)**2) 
+duma = sqrt(duma)
+titi = 2*degrees(atan((duma)))
+print('titi is...', titi)
+
+#*---------- ----------
+#*    elbow up
+#*---------- ----------
+TH2 = -degrees(atan2(dum1, dum2))
+print('theta2 from invK is...', TH2)
+
+cosAlpha = (x**2+y**2+l1**2-l2**2)/(2*l1*sqrt(x**2+y**2))
+alpha = degrees(atan2(sqrt(1-cosAlpha**2), cosAlpha))
+gamma = degrees(atan2(y, x))
+print('gamma from invK is...', gamma)
+TH1 = gamma+alpha
+print('theta1 from invK is...', TH1)
+
+
+
+
+
+
+
 
 
--- sympy/blockDiagram.py
+++ sympy/blockDiagram.py
@@ -9,20 +9,64 @@ import matplotlib.pyplot as plt
 from sympy.printing import pprint
 from sympy.matrices import Matrix
 from sympy          import simplify as simp
-
+from sympy.abc      import s
 
 def feedback (fw, lp):
     feedback = fw/(1+lp)
     return feedback
 
 
+#*---------- ----------
+#*    example 1
+#*---------- ----------
 g2, g3, h1, h2, h3 = sym.symbols('g2, g3, h1, h2, h3')
 
 tf1 = feedback(g2*g3, g2*g3*h1)
-pprint(tf1) 
+# pprint(tf1) 
 
 tf2 = feedback(tf1, tf1*(-h2))
-pprint(simp(tf2)) 
+# pprint(simp(tf2)) 
 
 tf3 = feedback(tf2, tf2*h3)
-pprint(simp(tf3)) 
+# pprint(simp(tf3)) 
+
+
+#*---------- ----------
+#*    problem 13 (7th) 
+#*---------- ----------
+b1 = 1/s+4
+b2 = feedback(5*s, 5*s)
+b3= b1*b2
+
+dumm1 = (3*s/5)+1
+b4= feedback(b3, b3*dumm1)
+# b5= feedback(b4, b4)
+pprint(simp(b4))
+
+#*---------- ----------
+#*    problem 8 (8th)
+#*---------- ----------
+# b1 = 1/s+3
+# b2 = feedback(2*s, 2*s)
+# b3= b1*b2
+
+# dumm1 = 4*s/2
+# b4= feedback(b3, b3*dumm1)
+# b5= feedback(b4, b4)
+# pprint(simp(b5))
+
+#*---------- ----------
+#*    exercise 5.1
+#*---------- ----------
+b1 = s*s+(1/s)
+b2 = 1/s
+b3= b1*b2
+
+dumm1 = 2*s
+b4= feedback(b3, b3*dumm1)
+pprint(simp(b4))
+
+
+
+
+
