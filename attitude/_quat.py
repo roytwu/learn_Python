e291c470d3d6e80745dcd1b0ef7d073955097b57
Roy <roywu2018@gmail.com>
2021-04-06 16:12:49 -0400

    show quaternion ambiguity

--- attitude/_quat.py
+++ attitude/_quat.py
@@ -1,9 +1,11 @@
 """
-File name:   _quat.py
-Developer:   wur
-Description: Symbolic computations to test funcitons of mapping between 
-             SO(3) and S(3) 
+Author:      Roy Wu
+Description: Symbolic computations of mapping between SO(3) and S(3) 
+History:     10/xx/2020 - initial version
+             04/06/2021 - ambibuity 
+             (difficulty of using tuple shall be fixed in the future)
 """
+
 import math
 import numpy as np
 from numpy.linalg   import norm
@@ -14,7 +16,7 @@ from sympy          import zeros
 from sympy.printing import pprint
 from sympy.matrices import Matrix
 from sympy          import simplify as simp
-
+from sympy          import nsimplify as nsimp
 
 th, phi, psi = symbols("theta phi psi")
 
@@ -34,30 +36,45 @@ def roundTinyDoubleToZero(Matx):
     return out        
 
 #* ----- Main program starts here -----
-xx = (1, 0, 0)
-yy = (0, 1, 0)
-zz = (0, 0, 1)
 
-#* creating random rotation matrix
+
+#*---------- ----------
+#*    creating random rotation matrices
+#*---------- ----------
 rotA = SO3.Rx(1.0)* SO3.Ry(1.0) 
-rotB = SO3.Rx(-math.pi/2.0)
-pprint(rotB)
+rotB = SO3.Rx(math.pi/6.0)
+print('rototation matrix is...')
+pprint(nsimp(rotB, tolerance=0.1))
 
-#* map SO(3) to S(3)
+
+#*---------- ----------
+#*    Map SO(3) to S^3
+#*---------- ----------
 quatA = SO3.SO3ToUnitQuat(rotA)
 quatB = SO3.SO3ToUnitQuat(rotB)
+print('\nquaternion is...')
 print(quatB)
 
-rotA2 = SO3.unitQuatToSO3(quatA)
-pprint(simp(rotA2))
+
+#*---------- ----------
+#*    Map S^3 to SO(3)
+#*---------- ----------
+# quatBn = (-quatB[0], -quatB[1], -quatB[2], -quatB[3]) #* ambiguity
+rotB2 = SO3.unitQuatToSO3(quatB)
+print('\nConverting S^3 back to SO(3)...')
+pprint(simp(rotB2))
 
 
-#* ----- testing quaternion multiplication -----
+
+
+#*---------- ----------
+#*    quaternion multiplication
+#*---------- ----------
 rotC = rotA*rotB                        #* multiplication of SO(3)
 quatC = S3.multiplication(quatA, quatB) #* multiplication of S(3)
 rotCC = SO3.unitQuatToSO3(quatC)
 
-print('\nShall obtain identity matrix... \n')
+print('\n\nShall obtain identity matrix... \n')
 result = rotCC* rotC.T 
-result = roundTinyDoubleToZero(result)
-pprint(result)
+out = nsimp(result, tolerance=0.001, rational=True)
+pprint(out)
