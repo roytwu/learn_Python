215cc5175e19e09cfc02b25ef8965347ff73456b
Roy <roywu2018@gmail.com>
2021-04-06 15:27:39 -0400

    adding more operations for RRP manipulator

--- homoXForm/main_RRP_np.py
+++ homoXForm/main_RRP_np.py
@@ -25,49 +25,65 @@ from homoTrans import vee    #* use custom module
 
 
 
-q1=q2=q3=0
+# q1=q2=q3=0
 r1= 13
 r2=6
 
-q1=0.3
-q2=0.3
-
+q1=np.radians(30)
+q2=np.radians(-90-16.7)
+q3=4.44
 
 
 # #*---------- ----------
 #*    forward kinematics
 #*---------- ----------
-T01 = hXform(0, np.pi/2, r1, np.pi/2+q1)
-T12 = hXform(0, np.pi/2, 0, np.pi+q2)
-T23 = hXform(0, 0, r2+q3, 0)
-T03 = multi_dot([T01, T12, T23]).round(2)
+T01 = hXform(0, np.pi/2, r1, np.pi/2+q1).round(5)
+T12 = hXform(0, np.pi/2, 0, np.pi+q2).round(5)
+T23 = hXform(0, 0, r2+q3, 0).round(5)
+T03 = multi_dot([T01, T12, T23]).round(3)
+print("T is...")
 pprint(T03)
 
 #*---------- ----------
 #*    Find the z axes for Jacobian
 #*---------- ----------
 #* extract SO(3) from SE(3)
-R01 = np.array(T01[0:3, 0:3]).round(2)
-R12 = np.array(T12[0:3, 0:3]).round(2)
-R23 = np.array(T23[0:3, 0:3]).round(2)
+R01 = np.array(T01[0:3, 0:3])
+R12 = np.array(T12[0:3, 0:3])
+R23 = np.array(T23[0:3, 0:3])
 
 
 o1 = np.array([[T01[0,3]], [T01[1,3]], [T01[2,3]]])
 o3 = np.array([[T03[0,3]], [T03[1,3]], [T03[2,3]]])
+print("o3 is...")
+pprint(o3)
 z = np.array([[0], [0], [1]])
 zero = np.array([ [0], [0], [0]])
 z1 = np.matmul(R01,z)
 z2 = multi_dot([R01, R12, z]).round(2)
-# pprint(z2)
+print("z is...")
+pprint(z2)
 
 Jd1 =np.matmul( hat(z), o3)
 Jd2 = np.matmul(hat(z1), o3-o1)
 Jd= np.hstack((Jd1, Jd2, z2)) #* horizontal matrix concatenation
-Jw= np.hstack((z, z1, zero)) #* horizontal matrix concatenation
+Jw= np.hstack((z, z1, zero))  #* horizontal matrix concatenation
 J= np.vstack((Jd, Jw))
-print(J)
+print("J is...", J)
 
 
+qdot = np.array([[0], [1], [0]])
+X = np.matmul(J, qdot)
+print("X is...")
+pprint(X)
 
 
+#*---------- ----------
+#*    Bonus
+#*---------- ----------
+p_C = np.array([[-4], [15], [3], [1]])
+T_0C= np.array([ [1,0,0,-5], [0,0,1,8.66], [0,-1,0,25], [0,0,0,1]])
+p_0 = multi_dot([T_0C, p_C]).round(5)
+print("p0 is...")
+pprint(p_0)
 
--- homoXForm/main_RRP_sym.py
+++ homoXForm/main_RRP_sym.py
@@ -5,7 +5,7 @@ History:     03/16/2021 - initial version
              
 """
 import numpy as np
-import numpy.lonalg.norm as norm
+# import numpy.linalg.norm as norm
 import sympy as sym
 from numpy.linalg   import multi_dot
 from sympy.printing import pprint
@@ -33,7 +33,7 @@ q3=4.44
 r1= 13
 r2=6
 q1=np.radians(30)
-q2=np.radians(106.7)
+q2=np.radians(16.7)
 
 # #*---------- ----------
 #*    forward kinematics
@@ -43,7 +43,7 @@ T12 = hXformSym(0, pi/2, 0, pi+q2)
 T23 = hXformSym(0, 0, r2+q3, 0)
 T03= T01*T12*T23
 #T = multi_dot([jt1, jt2, jt3, ht4]).round(3)
-pprint(simp(T03))
+# pprint(simp(T03))
 
 #*---------- ----------
 #*    Find the z axes for Jacobian
@@ -51,6 +51,7 @@ pprint(simp(T03))
 #* extract SO(3) from SE(3)
 R01 = Matrix([ T01[0:3, 0:3] ])
 R12 = Matrix([ T12[0:3, 0:3] ])
+# pprint(R12)
 R23 = Matrix([ T23[0:3, 0:3] ])
 
 o1 = Matrix([ T01[0:3, 3] ])
@@ -59,20 +60,20 @@ z = Matrix([ [0], [0], [1]])
 zero = Matrix([ [0], [0], [0]])
 z1 = R01*z
 z2 = R01*R12*z
-
+pprint(z2)
 
 Jd1 = hatSym(z)*(o3)
 Jd2 = hatSym(z1)*(o3-o1)
 Jd= Matrix.hstack(Jd1, Jd2, z2) #* horizontal matrix concatenation
 Jw= Matrix.hstack(z, z1, zero) #* horizontal matrix concatenation
 J= Matrix.vstack(Jd, Jw)
-# pprint(simp(J))
+pprint(simp(J))
 
-# qdot= Matrix([ [q1d], [0], [0]])
-qdot= Matrix([ [0], [q2d], [0]])
-# qdot= Matrix([ [0], [0], [q3d]])
-xdot = J*qdot
-pprint(simp(xdot))
+# # qdot= Matrix([ [q1d], [0], [0]])
+# qdot= Matrix([ [0], [q2d], [0]])
+# # qdot= Matrix([ [0], [0], [q3d]])
+# xdot = J*qdot
+# pprint(simp(xdot))
 
 
 
